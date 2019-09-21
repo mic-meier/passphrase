82ba9ff2f2075c5c57562772c6dffc927e0a2953
mic-meier <mic.l.meier@gmail.com>
2019-09-21 14:30:37 +0200

    Basic functionality with command line argument

--- helpers.py
+++ helpers.py
@@ -1,4 +1,14 @@
+from dice import D6
+
+
 def load_dict(file):
+    """
+    Loads a file with key - word combinations into a dict.
+    :param file: file path
+    :type file: .txt file with key and word separated by a tab
+    :return: dictionary with keys and words as values
+    :rtype: dict
+    """
     phrases = {}
 
     with open(file) as word_list:
@@ -11,3 +21,40 @@ def load_dict(file):
             phrases[words[0]] = words[1]
 
     return phrases
+
+
+def dice_throw(n):
+    """
+    Generates dictionary key by throwing n dice
+    :param n: Amount of dice to be thrown
+    :type n: int
+    :return: dice throw result, usable as dictionary key
+    :rtype: str
+    """
+    key = []
+    for i in range(n):
+        die = D6()
+        key.append(str(die.value))
+    return ''.join(key)
+
+
+def generate_passphrase(n, word_choices):
+    """
+    Generates passphrase, consisting of n words
+    :param n: Number of words the passphrase should consist of
+    :type n: int
+    :param word_choices: Dictionary of possible words
+    :type word_choices: dict
+    :return: Passphrase
+    :rtype: str
+    """
+    pass_phrase_list = []
+
+    for i in range(n):
+        key = dice_throw(5)
+        word = word_choices[key]
+        pass_phrase_list.append(word)
+
+    pass_phrase = ' '.join(pass_phrase_list)
+
+    return pass_phrase
--- passphrase_generator.py
+++ passphrase_generator.py
@@ -1,8 +1,31 @@
-from helpers import load_dict
+import sys
+from helpers import load_dict, generate_passphrase
 
 
 def main():
-    print(load_dict('eff_short_wordlist_1.txt'))
+
+    word_choices = load_dict('eff_large_wordlist.txt')
+
+    if len(sys.argv) == 2:
+        try:
+            amount_of_words = int(sys.argv[1])
+        except ValueError:
+            print('Usage: integer expected')
+            return 1
+
+    elif len(sys.argv) > 2:
+        print('Usage: Too many command line arguments')
+        return 2
+
+    else:
+        while True:
+            try:
+                amount_of_words = abs(int(input('Of how many words should your passphrase consist? ')))
+                break
+            except ValueError:
+                print('Please enter a positive whole number.')
+
+    print(generate_passphrase(amount_of_words, word_choices))
 
 
 if __name__ == '__main__':
